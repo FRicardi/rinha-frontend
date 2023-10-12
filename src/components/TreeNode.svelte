3fefb9665412d3bedceb6605454201ee2279448f
felipe ricardi <felipericardi5@gmail.com>
2023-10-12 18:15:26 -0300

    feat: displaying lines on structures

--- src/components/TreeNode.svelte
+++ src/components/TreeNode.svelte
@@ -1,18 +1,50 @@
 <script type="text/javascript">
   export let key = ""
   export let node;
-  export let depth = 0;
+  export let depth;
   
-  let isNodeLoopable = typeof node == 'object' || Array.isArray(node);
+  let isNodeLoopable = typeof node == 'object' || Array.isArray(node); 
+
+  function formatNode () {
+    if (!node)
+      return ''
+    
+    if (typeof node === 'string')
+      return `": ${node}"`;
+
+    if (Array.isArray(node))
+      return ":["
+
+    if (!isNodeLoopable)
+      return `: ${node}`;    
+
+    if (node)
+      return ':';   
+  }
 </script>
 
-<div style='padding-left: {depth * 20}px'>
-  {key}{!isNodeLoopable ? `: ${node}` : ""}
+<style type="text/css">
+  .tree-node { 
+    padding-left: 20px;
+  }
+
+  .inner-node {
+    border-left: 1px solid black;
+  }
+</style>
+
+
+<div class="tree-node {depth !== 0 ? 'inner-node' : ''}">
+  {key}{formatNode()}
+
+
+  {#if isNodeLoopable}
+    {#each Object.entries(node) as [nodeChildKey, nodeChild]}
+      <svelte:self key={nodeChildKey} node={nodeChild} depth={depth + 1} />
+    {/each}
+  {/if}
+
 </div>
-
-
-{#if isNodeLoopable && !!node}
-  {#each Object.entries(node) as [nodeChildKey, nodeChild]}
-    <svelte:self key={nodeChildKey} node={nodeChild} depth={depth + 1} />
-  {/each}
-{/if}
+  {#if Array.isArray(node)}
+    <svelte:self key="]" {depth} node={undefined} />
+  {/if}
